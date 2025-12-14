00bd178d91ccbae387503bd72e323068315ad8cb
erik-dunteman <erikdunteman@gmail.com>
2025-12-13 22:11:04 -0800

    caught wasm panic

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
     if (args.len != 2) {
-        try print("Usage: {any} <program>\n", .{args});
+        try print("Target file not provided\n", .{});
         return;
     }
     const program = try Program.init_from_file(allocator, args[1]);
@@ -40,6 +40,7 @@ const Program = struct {
     instructions: []u32,
 
     pub fn init_from_file(allocator: std.mem.Allocator, path: []const u8) !Program {
+        // TODO: panics in wasm
         const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
         defer file.close();
         const bytes = try file.readToEndAlloc(allocator, 1024 * 1024);
